89990344a56fa8dd2e1f6d125eb0eaaeb73cf912
beforesunset <1011908207@qq.com>
2022-09-02 14:19:38 +0800

    19 setup input polling

---
+++ Hazel/src/Hazel/Core/Input.h
@@ -0,0 +1,31 @@
+#pragma once
+#include "Hazel/Core/MouseCodes.h"
+#include "Hazel/Core/KeyCodes.h"
+
+// to be solved
+//#include <glm/glm.cpp>
+
+namespace Hazel
+{
+	class Input 
+	{
+	public:
+		static bool IsKeyPressed(KeyCode key);
+
+		static bool IsMouseButtonPressed(MouseCode button);
+		
+		// to be solved
+		//static glm::vec2 GetMousePosition();
+
+		static std::pair<float, float> GetMousePosition();
+
+		static float GetMouseX();
+		static float GetMouseY();
+
+	// to be solved
+	private:
+		static Input* s_Instance;
+	};
+
+
+}
---
+++ Hazel/src/Hazel/Platform/Windows/WindowsInput.cpp
@@ -0,0 +1,59 @@
+#pragma once
+#include "hzpch.h"
+#include "Hazel/Core/Input.h"
+
+#include "Hazel/Core/Application.h"
+
+#include <GLFW/glfw3.h>
+
+namespace Hazel
+{
+	Input* Input::s_Instance = new Input();
+
+	bool Input::IsKeyPressed(const KeyCode key)
+	{
+		auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetKey(window, static_cast<int32_t>(key));
+
+		return state == GLFW_PRESS;
+	}
+
+	bool Input::IsMouseButtonPressed(const MouseCode button)
+	{
+		auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		auto state = glfwGetMouseButton(window, static_cast<int32_t>(button));
+
+		return state == GLFW_PRESS;
+	}
+
+	// to be solved
+	/*glm::vec2 Input::GetMousePosition()
+	{
+		auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+
+	}*/
+
+	std::pair<float, float> Input::GetMousePosition()
+	{
+		auto* window = static_cast<GLFWwindow*>(Application::Get().GetWindow().GetNativeWindow());
+		double xpos, ypos;
+		glfwGetCursorPos(window, &xpos, &ypos);
+		
+		return { (float)xpos, (float)ypos };
+	}
+	float Input::GetMouseX()
+	{
+		auto [x, y] = GetMousePosition();
+		//return GetMousePosition().x;
+		return x;
+	}
+
+	float Input::GetMouseY()
+	{
+		auto [x, y] = GetMousePosition();
+		//return GetMousePosition().y;
+		return y;
+	}
+}
