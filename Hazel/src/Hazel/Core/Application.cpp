b47d477b2a4e2173998244dfeabe45efadaeebe3
Hazel <1011908207@qq.com>
2022-09-03 12:23:01 +0800

    fix up Application.cpp

---
+++ Hazel/src/Hazel/Core/Application.cpp
@@ -0,0 +1,81 @@
+#include "hzpch.h"
+#include "Application.h"
+#include "Hazel/Core/Log.h"
+#include "Hazel/Events/ApplicationEvent.h"
+
+#include "glad/glad.h"
+
+namespace Hazel
+{
+#define BIND_EVENT_FN(x) std::bind(&Application::x, this, std::placeholders::_1)
+
+	Application* Application::s_Instance = nullptr;
+
+	Application::Application()
+	{
+		HZ_CORE_ASSERT(!s_Instance, "Application already exists!");
+		s_Instance = this;
+		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(OnEvent));
+	}
+
+	Application::~Application()
+	{
+
+	}
+
+	void Application::Run()
+	{
+		WindowResizeEvent e(1280, 720);
+		if (e.IsInCategory(EventCategoryApplication))
+			HZ_INFO(e);
+
+		while (m_Running)
+		{
+			glClearColor(1, 0, 1, 1);
+			glClear(GL_COLOR_BUFFER_BIT);
+
+			for (Layer* layer : m_LayerStack)
+				layer->OnUpdate();
+			
+			m_Window->OnUpdate();
+		}
+	}
+
+	void Application::PushLayer(Layer* layer)
+	{
+		m_LayerStack.PushLayer(layer);
+		layer->OnAttach();
+	}
+
+	void Application::PushOverlay(Layer* overlay)
+	{
+		m_LayerStack.PushOverlay(overlay);
+		overlay->OnAttach();
+	}
+
+	void Application::OnEvent(Event& e)
+	{ 
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(OnWindowClose));
+		//dispatcher.Dispatch<WindowResizeEvent>(BIND_EVENT_FN(OnWindowResize));
+
+		//HZ_CORE_INFO("{0}", e);
+
+		for (auto it = m_LayerStack.rbegin(); it != m_LayerStack.rend(); ++it)
+		{
+			if (e.Handled)
+				break;
+			(*it)->OnEvent(e);
+		}
+
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+		return true;
+	}
+
+}
+
